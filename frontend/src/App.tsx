55c54187524ddfc317f207ed3b495ce0f18cb168
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-02-18 15:34:31 -0500

    Add homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import HowItWorks from "./pages/HowItWorks";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<HowItWorks />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/HowItWorks.tsx
@@ -0,0 +1,9 @@
+const HowItWorks = () => {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-r from-[#0D324D] to-[#7F5A83] text-white">
+      <h1>How It Works</h1>
+    </div>
+  );
+};
+
+export default HowItWorks;
