19063f7d12efa4596279de8cea628b427bb3c061
Shivam-J-07 <Shivamganeshjindal07@gmail.com>
2023-02-18 10:01:36 -0800

    Co-authored-by: Alyssa D'Souza <alyssadsouza@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-red-500"> Hello </p>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
