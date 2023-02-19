49c1ce611b3e5dbd2be928b7777e0627d39a24ad
Shivam-J-07 <Shivamganeshjindal07@gmail.com>
2023-02-19 12:12:38 -0800

    Added rating and img url to scraper

--- backend/search.py
+++ backend/search.py
@@ -3,4 +3,6 @@ from nltk_methods import get_adjectives, get_top_words
 
 example_scraper = AmznCrawler("Gengar plush stuffed animal")
 example_scraper.get_all_reviews()
+print(example_scraper.rating)
+print(example_scraper.img)
 print(get_top_words(get_adjectives(example_scraper.all_reviews), num_words=3))
