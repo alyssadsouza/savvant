210c1710874454ee33f46017faee7f0952934ba9
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-02-19 13:12:51 -0500

    Fix StarRating duplicate keys

--- frontend/src/components/StarRating.tsx
+++ frontend/src/components/StarRating.tsx
@@ -8,8 +8,8 @@ type Props = {
 export default function StarRating({ rating, classes }: Props) {
 	return (
 		<div className={`inline-flex gap-1 ${classes}`}>
-			{Array(Math.round(rating)).fill(0).map(index => <Star className="w-8 text-[#F1C117]" />)}
-			{Array(5 - Math.round(rating)).fill(0).map(index => <Star className="w-8 text-white/50" />)}
+			{Array.from(Array(Math.round(rating)).keys()).map(index => <Star key={index} className="w-8 text-[#F1C117]" />)}
+			{Array.from(Array(5 - Math.round(rating)).keys()).map(index => <Star key={index} className="w-8 text-white/50" />)}
 		</div>
 	)
 }
